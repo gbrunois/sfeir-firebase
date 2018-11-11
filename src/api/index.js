f4aca436b3e21dd929cd78cbb4149c9f2b08b221
GBrunois <brunois.g@sfeir.com>
2018-11-11 17:41:11 +0100

    :sparkles: install and add firebase configuration tagzytout: project-01-solution

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,17 @@
-function initializeApp() {}
+import firebase from "firebase/app";
+
+const config = {
+  apiKey: "AIzaSyChgGBAfNuisFjTAjMdsn2S2mkHWHUUQdA",
+  authDomain: "chfeirshare-firebase-demo.firebaseapp.com",
+  databaseURL: "https://chfeirshare-firebase-demo.firebaseio.com",
+  projectId: "chfeirshare-firebase-demo",
+  storageBucket: "chfeirshare-firebase-demo.appspot.com",
+  messagingSenderId: "692261967408"
+};
+
+function initializeApp() {
+  return firebase.initializeApp(config);
+}
 
 export default {
   initializeApp
