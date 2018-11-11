036d0d95c40454d83480ad1f671a528953cde4df
GBrunois <brunois.g@sfeir.com>
2018-11-11 15:26:00 +0100

    :sparkles: add vue components

--- sfeir-firebase/src/App.vue
+++ sfeir-firebase/src/App.vue
@@ -1,39 +1,52 @@
 <template>
   <v-app>
-    <v-toolbar app>
+    <v-toolbar app color="primary">
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>Rate a beer</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>open_in_new</v-icon>
-      </v-btn>
+      <v-menu bottom left>
+        <v-btn slot="activator" dark icon>
+          <v-icon>more_vert</v-icon>
+        </v-btn>
+        <v-list>
+          <v-list-tile @click="login" v-if="!isAuthenticated">
+            <v-list-tile-title>Login</v-list-tile-title>
+          </v-list-tile>
+          <v-list-tile @click="logout" v-if="isAuthenticated">
+            <v-list-tile-title>Logout</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import api from "@/api";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
+  name: "app",
+  data() {
     return {
-      //
+      isAuthenticated: api.isAuthenticated()
+    };
+  },
+  methods: {
+    login: function() {
+      api.authenticate();
+      this.$emit("loggedIn");
+      this.isAuthenticated = true;
+    },
+    logout: function() {
+      api.logout();
+      this.$emit("loggedOut");
+      this.isAuthenticated = false;
     }
   }
-}
+};
 </script>
---
+++ sfeir-firebase/src/components/BeerList.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-container fluid grid-list-lg>
+        <section>
+            <v-layout row wrap>
+                <v-flex xs12 v-for="(beer) in beers" :key="beer.id">
+                    <beer-item :beer="beer"></beer-item>
+                </v-flex>
+            </v-layout>
+        </section>
+    </v-container>
+</template>
+
+<script>
+import api from "@/api";
+import BeerItem from "@/components/BeerItem";
+
+export default {
+  name: "beer-list",
+  data: function() {
+    return {
+      beers: api.getBeers()
+    };
+  },
+  components: {
+    "beer-item": BeerItem
+  }
+};
+</script>
+
+<style scoped>
+section {
+  height: 100%;
+  margin-bottom: 60px;
+}
+</style>
