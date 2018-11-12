94e126885abddab2a7173a3bebb9056bd3d75268
GBrunois <brunois.g@sfeir.com>
2018-11-12 23:11:17 +0100

    :hammer: add console.debug

--- src/api/database.service.js
+++ src/api/database.service.js
@@ -41,14 +41,17 @@ function getBeers(onAddBeer, onUpdateBeer, onDeleteBeer) {
   dbRef.on("child_added", data => {
     const item = data.val();
     item.key = data.key;
+    console.debug(`onChildAdded ${JSON.stringify(item)}`);
     onAddBeer(item);
   });
   dbRef.on("child_changed", data => {
     const item = data.val();
     item.key = data.key;
+    console.debug(`onChildChanged ${JSON.stringify(item)}`);
     onUpdateBeer(item);
   });
   dbRef.on("child_removed", data => {
+    console.debug(`onChildRemoved ${JSON.stringify(data.key)}`);
     onDeleteBeer(data.key);
   });
 }
